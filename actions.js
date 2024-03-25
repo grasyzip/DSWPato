0433c9ae3b064a99b0b761335d31e2a8310271e5
Grasy R <159390727+grasyzip@users.noreply.github.com>
2024-03-25 20:30:27 -0300

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,33 @@
+<script>
+  
+  function raiva(){onclick='document.getElementById("myimage").src="patoraiva.jpg"'};
+raiva();
+
+  function timidez(){onclick='document.getElementById("myimage").src="patotimido.jpg'};
+timidez();
+			
+  function suave(){onclick='document.getElementById("myimage").src="patosuave.jpeg'};
+suave();
+
+  function triste(){onclick='document.getElementById("myimage").src="patotriste.jpg'};
+triste();
+
+  function chocade(){onclick='document.getElementById("myimage").src="chocade.webp'};
+chocade();
+
+  function soninho(){onclick='document.getElementById("myimage").src="soninho.jpg'};
+soninho();
+
+  function fomezinha(){onclick='document.getElementById("myimage").src="fomezinha.jpg'};
+fomezinha();
+			
+
+ #botao2func
+
+  function esconder(){
+onclick='document.getElementById("demo").style.display="block"'};
+esconder();
+
+  function aparecer(){
+onclick='document.getElementById("demo").style.fontSize="16px"'};
+aparecer();
